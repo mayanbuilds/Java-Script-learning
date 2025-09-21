569bc12db4a91b63cd74bf544d1c807bd8b41cb3
mayanbuilds <babaiayan90@gmail.com>
2025-09-20 22:26:23 -0700

    5 questions of python folder  done

---
+++ JS If Else/app.js
@@ -0,0 +1,40 @@
+// Pthon File Remaining Questions 
+
+//  Q8;
+var result1=prompt("enter marks of subject 1");
+console.log(result1);
+var result2=prompt("enter marks of subject 2");
+console.log(result2);
+var result3=prompt("enter marks of subject 3");
+console.log(result3);
+if(result1>=40 && result2>=40 && result3>=40){
+    console.log("Pass")
+}else{
+    console.log("fail")
+}
+
+
+// Q9:
+var time = 7;
+if(time>=6 && time <= 17){
+    console.log("light mode")
+}else{
+    console.log("dark mode")
+}
+
+
+// Q12:
+var mealidentifier = 6;
+if(mealidentifier>=6 && mealidentifier<=10){
+    console.log("Breakfast")
+}else if(mealidentifier>=12 && mealidentifier<=14){
+    console.log("Lunch")
+}else if(mealidentifier>=18 && mealidentifier<=21){
+    console.log(Dinner)
+}else{console.log("Snack")}
+
+
+// Python File Remaining Questions Done!!!!
+
+
+
